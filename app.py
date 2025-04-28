a75d2cd867212a2bb69b6ab290f776ed7dbdd6e8
Vyshnavi Mulakalapalli <vyshnavi.mulakalapalli@unh.edu>
2025-04-28 15:27:46 +0000

    add jobs to display on website and update html

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        "id": "1",
+        "title": "Software Engineer",
+        "location": "New York, United States",
+        "salary": "$100,000"
+    },
+    {
+        "id": "2",
+        "title": "Frontend Engineer",
+        "location": "San Francisco, United States",
+        "salary": "$120,000"
+    },
+    {
+        "id": "3",
+        "title": "Backend Engineer",
+        "location": "Chicago, United States",
+        "salary": "$90,000"
+    },
+    {
+        "id": "4",
+        "title": "Full Stack Engineer",
+        "location": "Missouri, United States",
+        "salary": "$95,000"
+    },
+]
+
+
 @app.route('/')
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS, company_name="Nexture")
+
+@app.route('api/jobs')
+def list_jobs():
+    return jsonify(jobs);
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
