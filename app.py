8df8132d56c63d57b761cde33d238080e458be70
Vyshnavi Mulakalapalli <vyshnavi.mulakalapalli@unh.edu>
2025-06-09 21:02:47 +0000

    add code to load a job from db

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
   jobs = load_jobs_from_db()
-  return render_template('home.html', jobs=jobs, company_name="Nexture")
+  return render_template('home.html', jobs=jobs)
 
 
 @app.route('/api/jobs')
@@ -20,7 +20,9 @@ def list_jobs():
 @app.route('/job/<id>')
 def show_job(id):
   job = load_job_from_db(id)
-  return jsonify(job)
+  if not job:
+    return "Not Found", 404
+  return render_template('jobpage.html', job=job)
 
 
 if __name__ == '__main__':
