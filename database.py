ceb0f2845b018fe5086e4688f6c9e0b90cfd448d
Vyshnavi Mulakalapalli <vyshnavi.mulakalapalli@unh.edu>
2025-06-13 21:44:06 +0000

    add code to save applications to database

--- database.py
+++ database.py
@@ -22,13 +22,32 @@ def load_jobs_from_db():
 
 def load_job_from_db(id):
     with engine.connect() as conn:
-        result = conn.execute(
-            text("select * from jobs where id = :val"),{"val":id})
+        result = conn.execute(text("select * from jobs where id = :val"),
+                              {"val": id})
         rows = result.all()
         if len(rows) == 0:
             return None
         else:
             return rows[0]._asdict()
+
+
+def add_application_to_db(job_id, data):
+    print("Data received: ", data)
+    with engine.begin() as conn:
+        query = text(
+            "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)"
+        )
+        conn.execute(
+            query, {
+                "job_id": job_id,
+                "full_name": data['full_name'],
+                "email": data['email'],
+                "linkedin_url": data['linkedin_url'],
+                "education": data['education'],
+                "work_experience": data['work_experience'],
+                "resume_url": data['resume_url']
+            })
+        print("Data inserted successfully")
 
 
 # print(type(result))
